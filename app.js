37d78387592b094381c21998a35c839341f99118
darrylgit <darrylsmithup@gmail.com>
2019-11-07 22:48:54 -0500

    Add GET and POST routes for tours

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
+const fs = require('fs');
 const express = require('express');
 
 const app = express();
 
+app.use(express.json());
+
+/*
 app.get('/', (req, res) => {
   res
     .status(200)
@@ -10,6 +14,54 @@ app.get('/', (req, res) => {
 
 app.post('/', (req, res) => {
   res.send('You can post to this endpoint');
+});
+*/
+
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
+
+app.get('/api/v1/tours', (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    results: tours.length,
+    data: { tours }
+  });
+});
+
+app.get('/api/v1/tours/:id', (req, res) => {
+  const id = req.params.id * 1; // multiplying by one converts string to number
+
+  if (id > tours.length) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID'
+    });
+  }
+
+  const tour = tours.find(el => el.id === id);
+
+  res.status(200).json({
+    status: 'success',
+    data: { tour }
+  });
+});
+
+app.post('/api/v1/tours', (req, res) => {
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+
+  tours.push(newTour);
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    err => {
+      res.status(201).json({
+        status: 'success',
+        data: { tour: newTour }
+      });
+    }
+  );
 });
 
 const port = 3000;
