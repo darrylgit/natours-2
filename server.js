957f919beae823382ede172dd05763aea6b5929b
darrylgit <darrylsmithup@gmail.com>
2019-11-09 22:37:16 -0500

    Install and set up ESLint

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+// eslint-disable-next-line import/newline-after-import
 const dotenv = require('dotenv');
 dotenv.config({ path: './config.env' });
 
@@ -5,5 +6,6 @@ const app = require('./app');
 
 const port = process.env.port || 3000;
 app.listen(port, () => {
+  // eslint-disable-next-line no-console
   console.log(`App running on port ${port}...`);
 });
