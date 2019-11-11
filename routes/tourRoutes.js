053c0899e8b8f0ebedd8f0f32d179986f0e92a35
darrylgit <darrylsmithup@gmail.com>
2019-11-11 17:31:41 -0500

    Refactor tours controller to not use local JSON

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const tourController = require('../controllers/tourController');
+
 const router = express.Router();
 
-router.param('id', tourController.checkID);
+// router.param('id', tourController.checkID);
 
 router
   .route('/')
--- server.js
+++ server.js
@@ -14,28 +14,11 @@ mongoose
   .connect(DB, {
     useNewUrlParser: true,
     useCreateIndex: true,
-    useFindAndModify: false
+    useFindAndModify: false,
+    useUnifiedTopology: true
   })
   // eslint-disable-next-line no-console
   .then(() => console.log('DB connnection successful!'));
-
-const tourSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, 'A tour must have a name'],
-    unique: true
-  },
-  rating: {
-    type: Number,
-    default: 4.5
-  },
-  price: {
-    type: Number,
-    required: [true, 'A tour must have a price']
-  }
-});
-
-const Tour = mongoose.model('Tour', tourSchema);
 
 const port = process.env.port || 3000;
 app.listen(port, () => {
