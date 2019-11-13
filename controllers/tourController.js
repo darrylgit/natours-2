9d31632675b4135ff111efb85be1561c7412248c
darrylgit <darrylsmithup@gmail.com>
2019-11-12 23:04:53 -0500

    Implement pagination queries

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -41,6 +41,18 @@ exports.getAllTours = async (req, res) => {
       query = query.select('-__v');
     }
 
+    // 4) Pagination
+    const page = req.query.page * 1 || 1;
+    const limit = req.query.limit * 1 || 100;
+    const skip = (page - 1) * limit;
+
+    query = query.skip(skip).limit(limit);
+
+    if (req.query.page) {
+      const numTours = await Tour.countDocuments();
+      if (skip >= numTours) throw new Error('This page does not exist');
+    }
+
     // EXECUTE QUERY
     const tours = await query;
 
