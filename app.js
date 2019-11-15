9c6c2a7932a425161e2bc447eae8dbe68093aa98
darrylgit <darrylsmithup@gmail.com>
2019-11-14 22:34:33 -0500

    Send JSON for unhandled routes

--- app.js
+++ app.js
@@ -14,13 +14,6 @@ if (process.env.NODE_ENV === 'development') {
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
 
-/*
-app.use((req, res, next) => {
-  console.log('Hello from the middleware 👋');
-  next();
-});
-*/
-
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   next();
@@ -29,5 +22,12 @@ app.use((req, res, next) => {
 // ROUTES
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
+
+app.all('*', (req, res, next) => {
+  res.status(404).json({
+    status: 'fail',
+    message: `Can't find ${req.originalUrl} on this server!`
+  });
+});
 
 module.exports = app;
