ba0eead4996ddd0889d5b65f1237cde5fa6d3111
darrylgit <darrylsmithup@gmail.com>
2019-11-17 13:21:00 -0500

    Specify fields in User.create

--- controllers/authController.js
+++ controllers/authController.js
@@ -2,7 +2,12 @@ const User = require('./../models/userModel');
 const catchAsync = require('./../utilities/catchAysnc');
 
 exports.signup = catchAsync(async (req, res, next) => {
-  const newUser = await User.create(req.body);
+  const newUser = await User.create({
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+    confirmPassword: req.body.confirmPassword
+  });
 
   res.status(201).json({
     status: 'success',
