d56f140785e04e9ed3af3e3433b2c3e1db2c37c0
darrylgit <darrylsmithup@gmail.com>
2019-11-22 23:39:27 -0500

    Add user ObjectIDs to tour schema

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,6 +1,5 @@
 const mongoose = require('mongoose');
 const slugify = require('slugify');
-const User = require('./userModel');
 //const validator = require('validator');
 
 const tourSchema = new mongoose.Schema(
@@ -109,7 +108,12 @@ const tourSchema = new mongoose.Schema(
         day: Number
       }
     ],
-    guides: Array
+    guides: [
+      {
+        type: mongoose.Schema.ObjectId,
+        ref: 'User'
+      }
+    ]
   },
   {
     toJSON: { virtuals: true },
@@ -127,11 +131,13 @@ tourSchema.pre('save', function(next) {
   next();
 });
 
+/*
 tourSchema.pre('save', async function(next) {
   const guidesPromises = this.guides.map(async id => await User.findById(id));
   this.guides = await Promise.all(guidesPromises);
   next();
 });
+*/
 
 /*
 tourSchema.pre('save', function(next) {
