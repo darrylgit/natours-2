a53406c99dcfd42fbfee4f539e58a1ce850a9e67
darrylgit <darrylsmithup@gmail.com>
2019-11-25 15:42:44 -0500

    Create factory function to create documents

--- controllers/handlerFactory.js
+++ controllers/handlerFactory.js
@@ -33,3 +33,13 @@ exports.updateOne = Model =>
       }
     });
   });
+
+exports.createOne = Model =>
+  catchAsync(async (req, res, next) => {
+    const newDoc = await Model.create(req.body);
+
+    res.status(201).json({
+      status: 'success',
+      data: { data: newDoc }
+    });
+  });
