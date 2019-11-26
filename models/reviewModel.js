7c96fcfe9c7dd4f67867b64a8b4966d819b5fe1b
darrylgit <darrylsmithup@gmail.com>
2019-11-26 12:12:43 -0500

    Clean console logs

--- models/reviewModel.js
+++ models/reviewModel.js
@@ -59,8 +59,6 @@ reviewSchema.statics.calcAverageRatings = async function(tourId) {
     }
   ]);
 
-  console.log(stats);
-
   if (stats.length > 0) {
     await Tour.findByIdAndUpdate(tourId, {
       ratingsQuantity: stats[0].nRatings,
@@ -82,7 +80,6 @@ reviewSchema.post('save', function() {
 
 reviewSchema.pre(/^findOneAnd/, async function(next) {
   this.r = await this.findOne();
-  console.log(this.r);
   next();
 });
 
