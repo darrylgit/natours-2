e0175ba423e9c4c1b029a59dd53a6f7008493168
darrylgit <darrylsmithup@gmail.com>
2019-11-28 14:32:10 -0500

    Create tour detail pages

--- controllers/viewController.js
+++ controllers/viewController.js
@@ -15,8 +15,15 @@ exports.getOverview = catchAsync(async (req, res, next) => {
   });
 });
 
-exports.getTour = (req, res) => {
-  res.status(200).render('tour', {
-    title: 'The Forest Hiker Tour'
+exports.getTour = catchAsync(async (req, res, next) => {
+  const { slug } = req.params;
+  const tour = await Tour.findOne({ slug }).populate({
+    path: 'reviews',
+    fields: 'review rating user'
   });
-};
+
+  res.status(200).render('tour', {
+    title: tour.name,
+    tour
+  });
+});
