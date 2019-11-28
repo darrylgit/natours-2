b2392e959814a8779a0a3b3b9959313124e85a17
darrylgit <darrylsmithup@gmail.com>
2019-11-27 21:45:37 -0500

    Refactor base pug with includes and extends

--- app.js
+++ app.js
@@ -70,8 +70,20 @@ app.use((req, res, next) => {
 // ROUTES
 app.get('/', (req, res) => {
   res.status(200).render('base', {
-    tour: 'The Forest Hiker',
+    title: 'Exciting tours for adventerous people',
     user: 'Daniel'
+  });
+});
+
+app.get('/overview', (req, res) => {
+  res.status(200).render('overview', {
+    title: 'All Tours'
+  });
+});
+
+app.get('/tour', (req, res) => {
+  res.status(200).render('tour', {
+    title: 'The Forest Hiker Tour'
   });
 });
 
