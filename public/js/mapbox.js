2d7c07e0b935149d04b7448a1d2de08fa853b162
darrylgit <darrylsmithup@gmail.com>
2019-11-28 21:23:20 -0500

    Add maps with mapbox

---
+++ public/js/mapbox.js
@@ -0,0 +1,49 @@
+/* eslint-disable */
+
+const locations = JSON.parse(document.getElementById('map').dataset.locations);
+
+console.log(locations);
+
+mapboxgl.accessToken =
+  'pk.eyJ1Ijoic2lnbmlvcmdyYXRpYW5vIiwiYSI6ImNrM2ppMWEwaTBqYzEzZWtvNzRzOWV6ZzAifQ.dSBe3phJn80pGvAOjBRU_w';
+var map = new mapboxgl.Map({
+  container: 'map',
+  style: 'mapbox://styles/signiorgratiano/ck3ji56b110nc1coalb9b3u2o',
+  scrollZoom: false
+});
+
+const bounds = new mapboxgl.LngLatBounds();
+
+locations.forEach(loc => {
+  // Create marker
+  const el = document.createElement('div');
+  el.className = 'marker';
+
+  // Add marker
+  new mapboxgl.Marker({
+    element: el,
+    anchor: 'bottom'
+  })
+    .setLngLat(loc.coordinates)
+    .addTo(map);
+
+  // Add popop
+  new mapboxgl.Popup({
+    offset: 30
+  })
+    .setLngLat(loc.coordinates)
+    .setHTML(`<p>Day ${loc.day}: ${loc.description}</p>`)
+    .addTo(map);
+
+  // Extend map bounds to include current location
+  bounds.extend(loc.coordinates);
+});
+
+map.fitBounds(bounds, {
+  padding: {
+    top: 200,
+    bottom: 150,
+    left: 100,
+    right: 100
+  }
+});
