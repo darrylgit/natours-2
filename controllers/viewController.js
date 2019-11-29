76c0d9a693b170ae02e920c4453d38d55b680fd2
darrylgit <darrylsmithup@gmail.com>
2019-11-29 13:56:48 -0500

    Toggle header based on login status

--- controllers/viewController.js
+++ controllers/viewController.js
@@ -5,10 +5,6 @@ exports.getOverview = catchAsync(async (req, res, next) => {
   // 1) Get tour data from collection
   const tours = await Tour.find();
 
-  // 2) Build template
-
-  // 3) Render that template using tour data
-
   res.status(200).render('overview', {
     title: 'All Tours',
     tours
