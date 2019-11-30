b4231e7e2dede4fc4971c530986759097335db30
darrylgit <darrylsmithup@gmail.com>
2019-11-30 16:22:46 -0500

    Prepare for revert

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ mongoose
   })
   .then(() => console.log('DB connnection successful!'));
 
-const port = process.env.port || 3001;
+const port = process.env.port || 3002;
 const server = app.listen(port, () => {
   console.log(`App running on port ${port}...`);
 });
