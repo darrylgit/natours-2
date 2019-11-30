27f71a82bba38bae7c8d416af3666ec98ec5f58f
darrylgit <darrylsmithup@gmail.com>
2019-11-30 15:06:13 -0500

    Enable logout

--- controllers/authController.js
+++ controllers/authController.js
@@ -70,6 +70,14 @@ exports.login = catchAsync(async (req, res, next) => {
   createSendToken(user, 200, res);
 });
 
+exports.logout = (req, res) => {
+  res.cookie('jwt', 'loggedout', {
+    expires: new Date(Date.now() + 10 * 1000),
+    httpOnly: true
+  });
+  res.status(200).json({ status: 'success' });
+};
+
 exports.protect = catchAsync(async (req, res, next) => {
   // 1) Get token, check if it's there
   let token;
@@ -111,32 +119,36 @@ exports.protect = catchAsync(async (req, res, next) => {
 });
 
 // Only for rendered pages, no errors!
-exports.isLoggedIn = catchAsync(async (req, res, next) => {
+exports.isLoggedIn = async (req, res, next) => {
   if (req.cookies.jwt) {
-    // 1) Verify token
-    const decoded = await promisify(jwt.verify)(
-      req.cookies.jwt,
-      process.env.JWT_SECRET
-    );
+    try {
+      // 1) Verify token
+      const decoded = await promisify(jwt.verify)(
+        req.cookies.jwt,
+        process.env.JWT_SECRET
+      );
 
-    // 2) Check if user still exists
-    const foundUser = await User.findById(decoded.id);
-    if (!foundUser) {
+      // 2) Check if user still exists
+      const foundUser = await User.findById(decoded.id);
+      if (!foundUser) {
+        return next();
+      }
+
+      // 3) Check if user changed password after the token was issued
+      if (foundUser.changedPasswordAfter(decoded.iat)) {
+        return next();
+      }
+
+      // THERE IS A LOGGED IN USER
+      res.locals.user = foundUser;
+      return next();
+    } catch (err) {
       return next();
     }
-
-    // 3) Check if user changed password after the token was issued
-    if (foundUser.changedPasswordAfter(decoded.iat)) {
-      return next();
-    }
-
-    // THERE IS A LOGGED IN USER
-    res.locals.user = foundUser;
-    return next();
   }
   console.log('No user found');
   next();
-});
+};
 
 exports.restrictTo = (...roles) => {
   return (req, res, next) => {
--- public/js/index.js
+++ public/js/index.js
@@ -1,11 +1,12 @@
 /* eslint-disable */
 import '@babel/polyfill';
 import { displayMap } from './mapbox';
-import { login } from './login';
+import { login, logout } from './login';
 
 // DOM ELEMENTS
 const mapBox = document.getElementById('map');
 const loginForm = document.querySelector('.form');
+const logOutBtn = document.querySelector('.nav__el--logout');
 
 // DELEGATION
 if (mapBox) {
@@ -23,3 +24,5 @@ if (loginForm) {
     login(email, password);
   });
 }
+
+if (logOutBtn) logOutBtn.addEventListener('click', logout);
