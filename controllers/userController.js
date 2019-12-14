63fbce3b87ffde9b9efd91635252d7d4e3f576a1
darrylgit <darrylsmithup@gmail.com>
2019-12-14 12:10:34 -0500

    Create image upload middleware with multer

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,7 +1,33 @@
+const multer = require('multer');
 const User = require('./../models/userModel');
 const catchAsync = require('./../utilities/catchAysnc');
 const AppError = require('./../utilities/appError');
 const factory = require('./handlerFactory');
+
+const multerStorage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, 'public/img/users');
+  },
+  filename: (req, file, cb) => {
+    const ext = file.mimetype.split('/')[1];
+    cb(null, `user-${req.user.id}-${Date.now()}.${ext}`);
+  }
+});
+
+const multerFilter = (req, file, cb) => {
+  if (file.mimetype.startsWith('image')) {
+    cb(null, true);
+  } else {
+    cb(new AppError('Not an image! Please upload an image.', 400), false);
+  }
+};
+
+const upload = multer({
+  storage: multerStorage,
+  fileFilter: multerFilter
+});
+
+exports.uploadUserPhoto = upload.single('photo');
 
 const filterObj = (obj, ...allowedFields) => {
   const newObj = {};
@@ -26,6 +52,7 @@ exports.updateMe = catchAsync(async (req, res, next) => {
 
   // 2) Filter out unwanted field names
   const filteredBody = filterObj(req.body, 'name', 'email');
+  if (req.file) filteredBody.photo = req.file.filename;
 
   // 3) Update user document
   const updatedUser = await User.findByIdAndUpdate(req.user.id, filteredBody, {
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -15,7 +15,11 @@ router.use(authController.protect);
 
 router.patch('/updatePassword', authController.updatePassword);
 router.get('/me', userController.getMe, userController.getUser);
-router.patch('/updateMe', userController.updateMe);
+router.patch(
+  '/updateMe',
+  userController.uploadUserPhoto,
+  userController.updateMe
+);
 router.delete('/deleteMe', userController.deleteMe);
 
 // Restrict all routes below this point to administrators
