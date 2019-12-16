a5a4bf77d915aa2a80a4da01950a20bbe51c2db2
darrylgit <darrylsmithup@gmail.com>
2019-12-16 13:14:31 -0500

    Remove development URLs

--- public/js/stripe.js
+++ public/js/stripe.js
@@ -7,9 +7,7 @@ const stripe = Stripe('pk_test_WVpLcRsuhgdNZDOCkLahPccl00KdwwKRAq');
 export const bookTour = async tourId => {
   try {
     // 1) Get session from API
-    const session = await axios(
-      `http://127.0.0.01:3000/api/v1/bookings/checkout-session/${tourId}`
-    );
+    const session = await axios(`/api/v1/bookings/checkout-session/${tourId}`);
     //console.log(session);
 
     // 2) Create checkout form
