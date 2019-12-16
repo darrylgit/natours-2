f1576488f60e082a7ff6741217025f3a6bfac20e
darrylgit <darrylsmithup@gmail.com>
2019-12-16 09:50:18 -0500

    Integrate Stripe on front end

---
+++ public/js/stripe.js
@@ -0,0 +1,23 @@
+/* eslint-disable */
+import axios from 'axios';
+import { showAlert } from './alerts';
+
+const stripe = Stripe('pk_test_WVpLcRsuhgdNZDOCkLahPccl00KdwwKRAq');
+
+export const bookTour = async tourId => {
+  try {
+    // 1) Get session from API
+    const session = await axios(
+      `http://127.0.0.01:3000/api/v1/bookings/checkout-session/${tourId}`
+    );
+    console.log(session);
+
+    // 2) Create checkout form
+    await stripe.redirectToCheckout({
+      sessionId: session.data.session.id
+    });
+  } catch (err) {
+    console.log(err);
+    showAlert('error', err);
+  }
+};
