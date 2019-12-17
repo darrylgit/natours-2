cfb70ae3f714ff98b5de7add476546ad0e163690
darrylgit <darrylsmithup@gmail.com>
2019-12-16 22:06:47 -0500

    Test for secure Heroku connections

--- app.js
+++ app.js
@@ -19,6 +19,8 @@ const globalErrorHandler = require('./controllers/errorController');
 
 const app = express();
 
+app.enable('trust proxy');
+
 app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views'));
 
